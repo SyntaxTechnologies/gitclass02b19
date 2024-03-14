d9d35a08eaceff0b704eb1176876d6f767481c7f
moazzamsadiq <moazzam@syntaxtechs.com>
2024-03-13 20:43:34 -0400

    knmflkdsnfmlkds;

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello mars");
         System.out.println("helloooooooo");
+        System.out.println("kdnwfwjhnfiernferiuhfnik");
 
     }
 }
